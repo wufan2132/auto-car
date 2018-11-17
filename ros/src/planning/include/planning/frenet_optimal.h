ec37aed799cc980653d1ba55e2db847ad5878163
吴凡 <35722552+wufan2132@users.noreply.github.com>
2018-11-17 14:32:23 +0800

    Create frenet_optimal.h

---
+++ ros/src/planning/include/planning/frenet_optimal.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <Eigen/Dense>
+#include <vector>
+#include<iostream>
+#include <math.h>
+#include "Interpolating.h"
+#include "Fitting.h"
+
+class car_status;
+
+class frenet_optimal
+{
+public:
+	frenet_optimal();
+	~frenet_optimal();
+	void process(Spline_Out& csp, car_status& status);
+
+};
+
+class car_status
+{
+public:
+	car_status();
+	~car_status();
+	float x_s;
+	float x_speed;
+	float x_acc;
+	float y_s;
+	float y_speed;
+	float y_acc;
+	
+};
+
+
