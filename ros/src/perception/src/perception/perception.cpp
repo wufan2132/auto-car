a96059aa8849d32309a7bda287dea271f6a6e6c6
wf <1475576386@qq.com>
2019-01-16 10:18:15 +0800

    get obstacle form lidar: add config

--- ros/src/perception/src/perception/perception.cpp
+++ ros/src/perception/src/perception/perception.cpp
@@ -4,8 +4,9 @@
 Car_Perception::Car_Perception(YAML::Node planning_conf)
 {
     conf.period = planning_conf["period"].as<double>();
+    conf.obstacle_source = planning_conf["obstacle_source"].as<string>();
     obstacleset = new ObstacleSet(planning_conf["ObstacleSet_conf"]);
-    lidarhandle = new LidarHandle(planning_conf);
+    lidarhandle = new LidarHandle(planning_conf["lidar_handle_conf"]);
 }
 
 void Car_Perception::pointcloud2_callback(const sensor_msgs::PointCloud2& msg){
@@ -14,6 +15,13 @@ void Car_Perception::pointcloud2_callback(const sensor_msgs::PointCloud2& msg){
 
 
 void Car_Perception::OnTimer(const ros::TimerEvent&){
-    obstacleset->refresh(pointcloud2_msg, lidarhandle);
+
+    if(conf.obstacle_source == "lidar")
+        obstacleset->refresh(pointcloud2_msg, lidarhandle);
+    else if(conf.obstacle_source == "reader")
+        obstacleset->refresh(obstacleset->conf.obstacle_path);
+    else 
+        ROS_ERROR("Car_Perception::OnTimer: conf.obstacle_source is error!");
+
     obstacle_publisher.publish(obstacleset->obstacle_message);
 }
