84287fe5ce978cd38838d882d2636d1edaa51f1b
wf <1475576386@qq.com>
2019-03-15 20:23:22 +0800

    fix some issue

--- carsim/README.md
+++
@@ -1,5 +0,0 @@
-carsim仿真
-
-car-model  模型文件
-
-udp-communication  udp通信接口文件
--- carsim/car-model/trash/UDP_close.m
+++
@@ -1,7 +0,0 @@
-function UDP_close()
-    global u
-    fclose(u);
-    delete(u);
-    echoudp('off');
-    delete(timerfind) 
-end
