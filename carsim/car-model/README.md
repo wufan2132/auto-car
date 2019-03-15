a1528e274f5549c1956ee88014dcb874b68b2504
wf <1475576386@qq.com>
2019-03-15 20:39:08 +0800

    fix some issue

--- carsim/car-model/README.md
+++
@@ -1,8 +0,0 @@
-文件类型说明：
-
-car.cpar：carsim模型文件	//file ->import 导入carsim软件中
-
-car.mdl：simulink模型文件
-
-
-TODO
--- carsim/car-model/trash/rece_callback.m
+++
@@ -1,7 +0,0 @@
-function rece_callback(~,~)
-    global rece_buf
-    global u
-    size = 32;
-    rece =char(fread(u, size));
-    rece_buf = sprintf(rece);
-end
