bbd937a2f0f891c3d8d3dc3f99878bf2d8228c2a
wufan <1475576386@qq.com>
2019-09-27 16:06:34 +0800

    -

---
+++ docker/images/images-log-wufan.md
@@ -0,0 +1,1 @@
+autocar:dev-x86_64-20190927_1557.tar  add_bazel_gtest_glog_qpsaoes
--- docker/images/release_images.sh
+++ docker/images/release_images.sh
@@ -1,2 +1,4 @@
-IMG_x86_64=autocar:dev-x86_64-20190926_1717.tar
+#
+IMG_x86_64=autocar:dev-x86_64-20190927_1557.tar
+#
 IMG_aarch64=autocar:dev-aarch64-20190926_2139.tar
