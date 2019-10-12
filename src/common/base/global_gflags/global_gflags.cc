7e77dd31fc1e2faa14c9fd94bf2c368e7f580b27
wufan <1475576386@qq.com>
2019-10-12 18:12:56 +0800

    -

--- src/common/base/global_gflags/global_gflags.cc
+++ src/common/base/global_gflags/global_gflags.cc
@@ -1,5 +1,8 @@
 
 #include "global_gflags.h"
 
-DEFINE_string(base_dir, "/autocar",
-              "program base dir");
+DEFINE_string(base_dir, "/autocar", "program base dir");
+
+DEFINE_string(system_run_mode, "gazebo", "real or gazebo");
+
+DEFINE_string(car_name, "none", "car name");
