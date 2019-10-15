269d6797ea48c4366871fdb8a5f83733b015dd05
wufan <1475576386@qq.com>
2019-10-15 17:10:15 +0800

    del stm32 src

--- stm32/Project/Browse/ff.d
+++
@@ -1,5 +0,0 @@
-.\browse\ff.o: ..\User\SDCard\ff.c
-.\browse\ff.o: ..\User\SDCard\ff.h
-.\browse\ff.o: ..\User\SDCard\integer.h
-.\browse\ff.o: ..\User\SDCard\ffconf.h
-.\browse\ff.o: ..\User\SDCard\diskio.h
--- stm32/User/Code_C/inc/Para.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PARA_H__
-#define __PARA_H__
-#include "stm32f4xx.h"
-#include "SBUS.h"
-
-
-#endif
